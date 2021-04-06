4c8f4a6f0e5e3cd02de31d9209e4ca358c634b51
Александр Емельянов <osvirt@gmail.com>
2021-04-06 21:37:55 +0700

    Тестовые данные брэндов и секций

---
+++ GB_lessons/Infrastructure/Services/Interfaces/IProductData.cs
@@ -0,0 +1,16 @@
+﻿using GB_lessons.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GB_lessons.Infrastructure.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+
+        IEnumerable<Brand> GetBrands();
+
+    }
+}
