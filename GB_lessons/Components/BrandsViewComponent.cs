3139bb27ed4d04520585df929b7b1ac5f403e124
Александр Емельянов <osvirt@gmail.com>
2021-04-06 22:29:01 +0700

    Визуальный компонент брэнда

---
+++ GB_lessons/Components/BrandsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using GB_lessons.Infrastructure.Services.Interfaces;
+using GB_lessons.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GB_lessons.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData productData) =>_ProductData = productData;
+
+        public IViewComponentResult Invoke() => View();
+
+        public IEnumerable<BrandViewModel> GetBrands() =>
+            _ProductData.GetBrands()
+                .OrderBy(b => b.Order)
+                .Select(b => new BrandViewModel
+                {
+                    Id = b.Id,
+                    Name = b.Name                    
+                });
+    }
+}
---
+++ GB_lessons/ViewModels/BrandViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GB_lessons.ViewModels
+{
+    public class BrandViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
