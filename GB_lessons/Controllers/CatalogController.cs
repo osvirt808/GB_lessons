c3177696e0e81b15af9c1485be2106f5f56640d6
Александр Емельянов <osvirt@gmail.com>
2021-04-07 21:55:26 +0700

    Контроллер каталога товаров

---
+++ GB_lessons/Controllers/CatalogController.cs
@@ -0,0 +1,43 @@
+﻿using GB_lessons.Domain;
+using GB_lessons.Infrastructure.Services.Interfaces;
+using GB_lessons.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace GB_lessons.Controllers
+{
+    public class CatalogController:Controller
+    {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData productData) => _ProductData = productData;
+
+        public IActionResult Index(int? BrandId, int? SectionId)
+        {
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            { 
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Products = products
+                    .OrderBy(p => p.Order)
+                    .Select(p => new ProductViewModel 
+                    { 
+                        Id = p.Id,
+                        Name = p.Name,
+                        Price = p.Price,
+                        ImageUrl = p.ImageUrl
+                    })
+            });
+
+            
+        }
+    }
+}
